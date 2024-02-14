b3cfe2f20ad69c7ff3b0495126ae2f98d2facd8c
Nikita1211-tech <155630110+Nikita1211-tech@users.noreply.github.com>
2024-02-14 22:40:37 +0530

    DB Changes

--- backend/src/model/userModel.js
+++ backend/src/model/userModel.js
@@ -1,53 +1,53 @@
-// const { DataTypes, NUMBER, INTEGER } = require("sequelize");
-// const { sequelize } = require("../dbconfig");
+const { DataTypes, NUMBER, INTEGER } = require("sequelize");
+const { sequelize } = require("../dbconfig");
 
-// const useBcrypt = require('sequelize-bcrypt');
-// const Users = sequelize.define('Users',{
-//     id:{
-//       type: DataTypes.NUMBER,
-//       primaryKey: true
-//     },
-//     firstname:{
-//       type: DataTypes.STRING,
-//       allowNull: false
-//     },
-//     lastname:{
-//       type: DataTypes.STRING,
-//       allowNull: false
-//     },
-//     email:{
-//         type: DataTypes.STRING,
-//         allowNull: false,
-//         unique: true,
-//     },
-//     profilepic:{
-//       type: DataTypes.STRING,
-//       allowNull: false,
-//       unique: true
-//     },
-//     password: {
-//       type: DataTypes.STRING,
-//       allowNull: false,
-//     },
-//     role:{
-//       type: DataTypes.STRING,
-//       allowNull: false,
-//       defaultValue: "User",
-//     },
-//     otp: {
-//       type: INTEGER,
-//       required: true,
-//       defaultValue: 0,
-//     },
-// })
-// const options = {
-//   field: 'password',
-//   rounds: 12,
-//   compare: 'authenticate', 
-// }
-// useBcrypt(Users, options);
-// Users.sync({ force: false }).then(() => {
-//     console.log('User table synced');
-// });
+const useBcrypt = require('sequelize-bcrypt');
+const Users = sequelize.define('Users',{
+    id:{
+      type: DataTypes.INTEGER,
+      primaryKey: true
+    },
+    firstname:{
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    lastname:{
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email:{
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    },
+    profilepic:{
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    role:{
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: "User",
+    },
+    otp: {
+      type: INTEGER,
+      allowNull: false,
+      defaultValue: 0,
+    },
+})
+const options = {
+  field: 'password',
+  rounds: 12,
+  compare: 'authenticate', 
+}
+useBcrypt(Users, options);
+Users.sync({ force: false }).then(() => {
+    console.log('User table synced');
+});
   
-// module.exports = Users;
+module.exports = Users;
