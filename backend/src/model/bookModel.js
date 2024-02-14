5e5fa2c9c7d7d7cc41f39148aa6b068c6693a26d
Nikita <nikita.k@codestoresolutins.com>
2024-02-14 17:44:07 +0530

    DB changes

--- backend/src/model/bookModel.js
+++ backend/src/model/bookModel.js
@@ -1,68 +1,68 @@
-const { DataTypes } = require("sequelize");
-const { sequelize } = require("../dbconfig");
+// const { DataTypes } = require("sequelize");
+// const { sequelize } = require("../dbconfig");
 
 
-const Book = sequelize.define('Book',{
-    id: {
-        type: DataTypes.NUMBER,
-        primaryKey: true,
-        autoIncrement: true,
-        allowNull: false,
-    },
-    img: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-    },
-    bookName: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    }, 
-    writerName: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    sellingprice: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    costprice: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    publishyear: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    booksummary: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    author: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    bookcat_img: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-    },
-    category: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    booktype_img: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-    },
-    booktypename: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    }
-})
-Book.sync({ force: false }).then(() => {
-    console.log('Book table synced');
-});
+// const Book = sequelize.define('Book',{
+//     id: {
+//         type: DataTypes.NUMBER,
+//         primaryKey: true,
+//         autoIncrement: true,
+//         allowNull: false,
+//     },
+//     img: {
+//         type: DataTypes.STRING,
+//         allowNull: false,
+//         unique: true,
+//     },
+//     bookName: {
+//       type: DataTypes.STRING,
+//       allowNull: false,
+//     }, 
+//     writerName: {
+//         type: DataTypes.STRING,
+//         allowNull: false,
+//     },
+//     sellingprice: {
+//         type: DataTypes.STRING,
+//         allowNull: false,
+//     },
+//     costprice: {
+//         type: DataTypes.STRING,
+//         allowNull: false,
+//     },
+//     publishyear: {
+//         type: DataTypes.STRING,
+//         allowNull: false,
+//     },
+//     booksummary: {
+//         type: DataTypes.STRING,
+//         allowNull: false,
+//     },
+//     author: {
+//         type: DataTypes.STRING,
+//         allowNull: false,
+//     },
+//     bookcat_img: {
+//         type: DataTypes.STRING,
+//         allowNull: false,
+//         unique: true,
+//     },
+//     category: {
+//         type: DataTypes.STRING,
+//         allowNull: false,
+//     },
+//     booktype_img: {
+//         type: DataTypes.STRING,
+//         allowNull: false,
+//         unique: true,
+//     },
+//     booktypename: {
+//         type: DataTypes.STRING,
+//         allowNull: false,
+//     }
+// })
+// Book.sync({ force: false }).then(() => {
+//     console.log('Book table synced');
+// });
   
-module.exports = Book;
+// module.exports = Book;
