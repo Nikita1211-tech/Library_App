3d3dc16eebea23b783faad113d3605a5a05ca66f
Nikita1211-tech <155630110+Nikita1211-tech@users.noreply.github.com>
2024-02-14 09:01:12 +0530

    Form Changes

--- client/src/app/data/interfaces/register.interface.ts
+++ client/src/app/data/interfaces/register.interface.ts
@@ -1,6 +1,5 @@
 export interface register{
-    firstname: string,
-    lastname: string,
+    username: string,
     email: string,
     profilepic: string,
     password: string,
--- client/src/app/modules/user/register/register.component.ts
+++ client/src/app/modules/user/register/register.component.ts
@@ -13,25 +13,47 @@ import { Router } from '@angular/router';
 })
 export class RegisterComponent {
   registerform: FormGroup
+  otpform: FormGroup
+  passwordform: FormGroup
   constructor(private fb: FormBuilder, private register: RegisterService, private router: Router){
     this.registerform = new FormGroup({
-      firstname: new FormControl('', [Validators.required]),
-      lastname: new FormControl('', [Validators.required]),
+      username: new FormControl('', [Validators.required]),
       email: new FormControl('', [Validators.required, Validators.email]),
-      profilepic: new FormControl('', [Validators.required]),
-      password: new FormControl('', [Validators.required]),
-      checkpassword: new FormControl('', [Validators.required])
+      // profilepic: new FormControl('', [Validators.required]),
+      // password: new FormControl('', [Validators.required]),
+      // checkpassword: new FormControl('', [Validators.required])
     })
+    this.otpform = new FormGroup({
+      otp: new FormControl('', Validators.required),
+    })
+    this.passwordform = new FormGroup({
+      
+    })
+  }
+  showOtpForm(): void {
+    const registerForm = document.getElementById("registerForm");
+    const resetForm = document.getElementById("resetPasswordForm");
+    const updateForm = document.getElementById("updatePasswordForm");
+    // if(resetForm && updateForm){
+    //   resetForm.style.display = "none";
+    //   updateForm.style.display = "none";
+    // } else {
+    //   console.error("Element with id 'resetPasswordForm' not found");
+    // }
+    // if(otpForm) {
+    //   otpForm.style.display = "flex";
+    // } else {
+    //   console.error("Element with id 'otpPasswordForm' not found");
+    // }
   }
   onRegister(): void{
    const obj:register = {
-       firstname:  this.registerform.value.firstname,
-       lastname: this.registerform.value.lastname,
+       username:  this.registerform.value.username,
        profilepic: this.registerform.value.profilepic,
        email:  this.registerform.value.email,
        password: this.registerform.value.password,
        role: "User"
-   } 
+   }
    this.register.register(obj, (error) => {
     if(error?.error?.message === "User already exists"){
       // Swal.fire({
