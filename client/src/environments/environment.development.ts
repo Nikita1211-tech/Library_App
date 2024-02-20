3ccdc803d1feebedec5e9823eabcc5553581b445
Nikita <nikita.k@codestoresolutins.com>
2024-02-20 16:34:21 +0530

    Changes

--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    // API_URL: 'https://library-backend-cuh0.onrender.com/api/users',
-    API_URL: 'http://localhost:3000/api/users',
+    API_URL: 'https://library-backend-cuh0.onrender.com/api/users',
+    // API_URL: 'http://localhost:3000/api/users',
     IMG_URL: 'http://localhost:3000/',
 };
