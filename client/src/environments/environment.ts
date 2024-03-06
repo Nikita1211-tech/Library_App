88b976a9d7aa438dd350abbaa17b7b94ebc0ee65
Nikita <nikita.k@codestoresolutins.com>
2024-03-06 13:15:55 +0530

    Api url changes

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
-    // API_URL: 'https://library-backend-cuh0.onrender.com/api/users',
-    API_URL: 'http://localhost:3000/api/users',
-    IMG_URL: 'localhost:3000/',
-    // IMG_URL: 'https://library-backend-cuh0.onrender.com/'
+    API_URL: 'https://library-backend-cuh0.onrender.com/api/users',
+    // API_URL: 'http://localhost:3000/api/users',
+    // IMG_URL: 'localhost:3000/',
+    IMG_URL: 'https://library-backend-cuh0.onrender.com/'
 };
